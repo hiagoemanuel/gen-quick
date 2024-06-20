63c67c19da9c0fa9eb7044c9e77ad579ff6fc8b4
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-06-20 10:49:58 -0300

    docs: add media

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      dark: '#09090B',
+      light: '#FAFAFA',
+      grey: '#27272A',
+    },
     extend: {},
   },
   plugins: [],
